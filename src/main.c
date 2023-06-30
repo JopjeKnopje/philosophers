a4117ee537e16bd55edf0050a0a59c26fcbb9137
JopjeKnopje <jboeve009@gmail.com>
2023-06-30 13:04:47 +0200

    learning time thing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/06/21 16:29:24 by joppe         #+#    #+#                 */
-/*   Updated: 2023/06/30 12:47:47 by joppe         ########   odam.nl         */
+/*   Updated: 2023/06/30 13:04:04 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,76 +14,37 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <threads.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 #include "philo.h"
 
-#define FORK_COUNT 5
-
-void *routine(void *arg)
-{
-	t_philo *philo = arg;
-
-	print_philo(philo);
-
-	int i = 0;
-	while (i < 10)
-	{
-		printf("i: %d\n", i);
-		i++;
-	}
-	printf("done\n");
-	
-	return (NULL);
-}
-
 int main(int argc, char *argv[]) 
 {
-	t_meta meta;
 
-	meta.philo_count = FORK_COUNT;
-	forks_init(&meta, meta.philo_count);
-	philos_init(&meta, meta.philo_count);
-
-
-	int i = 0;
-	meta.threads = ft_calloc(sizeof(pthread_t *), meta.philo_count);
-	while (i < meta.philo_count)
-	{
-		meta.threads[i] = thread_init(&meta, &routine, meta.philos[i]);
-		if (!meta.threads[i])
-		{
-			printf("error creating threads\n");
-			return (EXIT_FAILURE);
-		}
-		usleep(1 * 1000000 / 2);
-		i++;
-	}
-
-	struct timeval tv;
+	struct timeval tv1;
+	struct timeval tv2;
 	struct timezone tz;
-	tz.tz_dsttime = 0;
-	tz.tz_minuteswest = 0;
-	long delta = 0;
 
-	gettimeofday(&tv, &tz);
+	bzero(&tv1, sizeof(struct timeval));
+	bzero(&tv2, sizeof(struct timeval));
+	bzero(&tz, sizeof(struct timezone));
 
-	delta = tv.tv_usec;
-	printf("useconds %ld\n", tv.tv_usec);
-	// usleep(1 * 1000000);
-	sleep(3);
+	signed long delta;
 
-	gettimeofday(&tv, &tz);
+	gettimeofday(&tv1, &tz);
 
-	delta = tv.tv_usec - delta;
-	printf("useconds %ld\n", tv.tv_usec);
+	usleep(1000);
+	// sleep(1);
+
+	gettimeofday(&tv2, &tz);
+	delta = (tv2.tv_usec) - (tv1.tv_usec);
+	// delta = (tv2.tv_sec + tv2.tv_usec) - (tv1.tv_sec + tv1.tv_usec);
 	printf("delta %ld\n", delta);
 
 
-	free_threads(&meta);
-	free_philos(&meta);
-	free_forks(&meta);
+
 	return (0);
 }
