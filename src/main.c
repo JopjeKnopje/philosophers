a997b6ee5d1194aa08fa56a97d0eae9b954fa3f7
JopjeKnopje <jboeve009@gmail.com>
2023-07-07 21:18:24 +0200

    added new timer

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>             +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/06/21 16:29:24 by joppe         #+#    #+#                 */
-/*   Updated: 2023/07/07 00:50:10 by joppe         ########   odam.nl         */
+/*   Updated: 2023/07/07 21:17:46 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
 #include <threads.h>
 #include <sys/time.h>
 #include <time.h>
@@ -22,20 +21,57 @@
 #include "philo.h"
 #include "timer.h"
 
+
+#define FORK_COUNT 5
+
+void *routine(void *arg)
+{
+	t_philo *philo = arg;
+
+	print_philo(philo);
+
+	int i = 0;
+	while (i < 10)
+	{
+		printf("i: %d\n", i);
+		i++;
+	}
+	printf("done\n");
+	
+	return (NULL);
+}
+
 int main(int argc, char *argv[]) 
 {
-	t_timer *timer = timer_init();
+	t_meta meta;
 
-	timer_start(timer);
-
-	// sleep(1);
-	usleep(100000);
-
-	t_timeval *delta = timer_delta(timer);
-	printf("d s %ld | d us %ld\n", delta->tv_sec, delta->tv_usec);
-	timer_free(timer);
+	meta.philo_count = FORK_COUNT;
+	forks_init(&meta, meta.philo_count);
+	philos_init(&meta, meta.philo_count);
 
 
+	int i = 0;
+	meta.threads = ft_calloc(sizeof(pthread_t *), meta.philo_count);
+	while (i < meta.philo_count)
+	{
+		meta.threads[i] = thread_init(&meta, &routine, meta.philos[i]);
+		if (!meta.threads[i])
+		{
+			printf("error creating threads\n");
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
 
+	t_timer *t = timer_init();
+
+	usleep(1000000);
+	t_timeval *delta = timer_delta(t);
+	printf("delta %ld\n", delta->tv_usec);
+	timer_free(t);
+
+	free_threads(&meta);
+	free_philos(&meta);
+	free_forks(&meta);
 	return (0);
 }
